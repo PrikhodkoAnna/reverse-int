1bba4a3d210e0886143356272241f60f62ae8680
PrikhodkoAnna <prikhodkoannav@gmail.com>
2021-07-12 23:49:40 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    if (n < 0) {
+        n = Math.abs(n);
+    }
+        let str = '' + n;
+        let newStr = '';
+        for (let i = str.length - 1; i >= 0; i--) {
+            newStr += str[i];
+        }
+        let newNum = + newStr
+        return newNum;
 }
